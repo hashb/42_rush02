c5f83c05818545b1945301f2735eb5c2cad5f767
Nicholas Kirkby <nkirkby@e1z1r5p9.42.us.org>
2018-10-06 23:41:22 -0700

    adding potential match struct type

--- ex00/include/rush0.h
+++ ex00/include/rush0.h
@@ -6,7 +6,7 @@
 /*   By: nkirkby <nkirkby@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/06 20:09:08 by nkirkby           #+#    #+#             */
-/*   Updated: 2018/10/06 20:18:36 by nkirkby          ###   ########.fr       */
+/*   Updated: 2018/10/06 23:40:43 by nkirkby          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,52 @@
 #ifndef RUSH0_H
 # define RUSH0_H
 
-enum					e_rush0
+
+typedef struct	s_potential_match
 {
-	RUSH01,
-	RUSH02,
-	RUSH03,
-	RUSH04
+	int 		(* is_valid)(char *);
+	char 		**name;
+}				t_potential_match;
+
+int is_rush00_valid(char *stdin);
+int is_rush01_valid(char *stdin);
+int is_rush02_valid(char *stdin);
+int is_rush03_valid(char *stdin);
+int is_rush04_valid(char *stdin);
+int is_square_valid(char *stdin);
+
+
+#define NUMBER_OF_THINGS_WE_CAN_MATCH 5
+
+t_potential_match g_matches[] = {
+	{ .is_valid = &is_rush00_valid, .name = "rush 00"}, // not valid
+	{ .is_valid = &is_rush01_valid, .name = "rush 01"}, // valid
+	{ .is_valid = &is_rush02_valid, .name = "rush 02"}, // valid
+	{ .is_valid = &is_rush03_valid, .name = "rush 03"}, // not valid
+	{ .is_valid = &is_rush04_valid, .name = "rush 04"}, // not valid
+	{ .is_valid = &is_square_valid, .name = "square"}, // valid
 };
 
-#endif
+int main()
+{
+	int i; 
+	int has_already_printed_something;
+
+	i = 0;
+	has_already_printed_something = 0;
+	while (i < NUMBER_OF_THINGS_WE_CAN_MATCH)
+	{
+		if (g_matches[i].is_valid(stdin))
+		{
+			if (has_already_printed_something)
+				ft_putstr(" || ");
+			else
+				has_already_printed_something = 1;
+			ft_putstr(g_matches[i].name);
+		}
+		i++;
+	}
+}
+
+
 

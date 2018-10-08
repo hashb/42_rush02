689191712aeb3015ca48665a5a5c395d939630b3
Nicholas Kirkby <nkirkby@e1z1r6p27.42.us.org>
2018-10-07 17:10:14 -0700

    got 3 maybe

--- ex00/include/rush0.h
+++ ex00/include/rush0.h
@@ -6,7 +6,7 @@
 /*   By: nkirkby <nkirkby@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/06 20:09:08 by nkirkby           #+#    #+#             */
-/*   Updated: 2018/10/07 16:23:48 by nkirkby          ###   ########.fr       */
+/*   Updated: 2018/10/07 16:49:25 by nkirkby          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,14 @@ char				rush00(int width, int height, int x, int y);
 // int					rush01(char *arr, int w, int h);
 // int					rush02(char *arr, int w, int h);
 // int					rush03(char *arr, int w, int h);
+char				rush03(int width, int height, int x, int y);
 char				rush04(int width, int height, int x, int y);
 
 t_potential_match	g_matches[] = {
 	{ .reference_char = &rush00, .name = "rush-00"},
 	// { .reference_char = &rush01, .name = "rush-01"},
 	// { .reference_char = &rush02, .name = "rush-02"},
-	// { .reference_char = &rush03, .name = "rush-03"},
+	{ .reference_char = &rush03, .name = "rush-03"},
 	{ .reference_char = &rush04, .name = "rush-04"},
 };
 
--- ex00/src/r03.c
+++ ex00/src/r03.c
@@ -6,20 +6,17 @@
 /*   By: nkirkby <nkirkby@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/23 11:17:40 by eubotnar          #+#    #+#             */
-/*   Updated: 2018/10/07 15:52:15 by nkirkby          ###   ########.fr       */
+/*   Updated: 2018/10/07 17:09:34 by nkirkby          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	rush03(int width, int height, int x, int y)
 {
-	if (x == 1 && y == 1)
+	if ((x == 1 && y == 1) || (x == width && y == height))
 		return ('A');
-	else if (width == x && (height == y || height == 1))
+	if ((x == width && y == 1) || (y == height && x == 1))
 		return ('C');
-	else if (x == 1 && (height == y || height == 1))
-		return ('A');
-	else if ((width == 1) || (height == y) || (height == 1) || (width == x))
+	if ((x == 1) || (x == width) || (y == 1) || (y == height))
 		return ('B');
-	else 
-		return (' ');
+	return (' ');
 }

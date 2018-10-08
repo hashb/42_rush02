399a8daf6569da622497960fdc5541f286fb1c27
Chenna Kautilya <kchenna@student.42.us.org>
2018-10-07 17:44:57 -0700

    updated grep

--- .tests/tests_runner.sh
+++ .tests/tests_runner.sh
@@ -3,14 +3,14 @@
 #                                                         :::      ::::::::    #
 #    tests_runner.sh                                    :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: nkirkby <nkirkby@student.42.fr>            +#+  +:+       +#+         #
+#    By: kchenna <kchenna@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/10/07 16:31:46 by kchenna           #+#    #+#              #
-#    Updated: 2018/10/07 17:32:32 by nkirkby          ###   ########.fr        #
+#    Updated: 2018/10/07 17:42:42 by kchenna          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-set -e
+# set -e
 
 for i in {0..4}; do gcc -o z_rush0$i ./rush00_sources/rush0$i.c ../ex00/src/ft_utils.c  -I ../ex00/include; done
 
@@ -26,7 +26,7 @@ do
 			echo "********************"
 			echo ""
 			echo "rush 0$r and i: $i  j: $j"
-			./z_rush0$r $i $j | ../ex00/colle-2
+			./z_rush0$r $i $j | ../ex00/colle-2 | grep -F "[rush-0$r] [$i] [$j]"
 		done
 	done
 done
